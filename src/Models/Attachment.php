ef964fee850014054b5481b0aadd0c4ca87bc951
Xurshiddd <marifovxurshid800@gmailcom>
2025-09-02 12:53:31 +0500

    add reletions

--- src/Models/Attachment.php
+++ src/Models/Attachment.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace Xurshiddd\LaravelAttachment\Models;
-
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Attachment extends Model
 {
@@ -18,4 +21,12 @@ class Attachment extends Model
         'type',
         'attachment'
     ];
+    public function url(): Attribute
+    {
+        return Attribute::make(fn(): string => URL::to('storage/' . $this->path));
+    }
+    public function attachment(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
